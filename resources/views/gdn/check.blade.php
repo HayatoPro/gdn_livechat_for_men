3bbfd278197bb0219bdda75878122134e2462e57
EC2 Default User <ec2-user@ip-172-31-3-105.us-east-2.compute.internal>
2020-10-09 07:08:33 +0000

    Mm

--- resources/views/gdn/check.blade.php
+++ resources/views/gdn/check.blade.php
@@ -10,7 +10,8 @@
     </head>
     <body>
         <br>
-        <img src="{{ asset('image/tw_app.png') }}"></img>
-        <p>公式ストアへ遷移します。再度<a href="https://apps.apple.com/jp/app/%E3%83%90%E3%82%A4%E3%83%88-%E3%82%A2%E3%83%97%E3%83%AA%E3%81%AF%E3%82%BF%E3%82%A6%E3%83%B3%E3%83%AF%E3%83%BC%E3%82%AF/id434586962">コチラ</a>をタップして下さい。</p>
+        <h1>応募多数！スキマ時間を活かそう！</h1>
+        <img src="{{ asset('image/tm_c1.png') }}"></img>
+        <p>公式ストアへ遷移します。再度<a href="https://apps.apple.com/jp/app/%E3%82%BF%E3%82%A4%E3%83%9F%E3%83%BC-%E3%81%99%E3%81%90%E3%81%AB%E5%83%8D%E3%81%91%E3%81%A6%E3%81%99%E3%81%90%E3%81%AB%E3%81%8A%E9%87%91%E3%81%8C%E3%82%82%E3%82%89%E3%81%88%E3%82%8B/id1409383333">コチラ</a>をタップして下さい。</p>
     </body>
 </html>
