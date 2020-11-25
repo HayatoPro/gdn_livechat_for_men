6c11e0e62d054b4a042fe771914915f5ba8b07ef
EC2 Default User <ec2-user@ip-172-31-3-105.us-east-2.compute.internal>
2020-11-25 19:54:10 +0000

    mm

---
+++ resources/views/gdn/newJob.blade.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>公式ページヘ移動してください。</title>
+        <link href="{{ secure_asset('css/newJob.css') }}" rel="stylesheet">
+        <link href="https://use.fontawesome.com/releases/v5.6.1/css/all.css" rel="stylesheet">
+    </head>
+    <body>
+        <img src="{{ asset('image/lp_instagram_job.png') }}" class="wide">
+        <h1>見つけたあなたはラッキー!?<br><span class="red">話題の在宅ワーク</span>で、<br>空いた時間をお金に換えよう!</h1>
+        <div class="button">
+            <a href="https://a-trade.jp/redirect/giftchat?media=G16531" target="_blank" class="btn-square-little-rich">専用アプリはコチラ <i class="fas fa-chevron-circle-right"></i></a>
+        </div>
+        <p>※詳細は<span class="red">アプリ内をCHECK!</span></p>
+        <!--<br>-->
+        <img src="{{ asset('image/jobManga_1.png') }}" class="wide">
+        <img src="{{ asset('image/jobManga_2.png') }}" class="wide">
+        <h1>有名雑誌 & 女性人気インフルエンサーも紹介!?<br><span class="red">巷で話題の在宅ワーク</span>はコチラ!</h1>
+        <div class="button">
+            <a href="https://a-trade.jp/redirect/giftchat?media=G16531" target="_blank" class="btn-square-little-rich">専用アプリはコチラ <i class="fas fa-chevron-circle-right"></i></a>
+        </div>
+        <p>※詳細は<span class="red">アプリ内をCHECK!</span></p>
+    </body>
+</html>
