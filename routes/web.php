2ddc2ab32750a91b60dab68fa9b681761e11b093
EC2 Default User <ec2-user@ip-172-31-3-105.us-east-2.compute.internal>
2021-02-03 11:59:19 +0000

    M

--- routes/web.php
+++ routes/web.php
@@ -12,8 +12,8 @@
 */
 // LP獲得用
 Route::get('new', function () {
-    return view('gdn.check'); //提出用
-    // return redirect('https://a-trade.jp/redirect/mocom-lp30?media=G16531'); //モコム - Aトレード
+    // return view('gdn.check'); //提出用
+    return redirect('https://a-trade.jp/redirect/mocom-lp30?media=G16531'); //モコム - Aトレード
     
     // return view('gdn.papa'); //獲得用(6) - パパ活
     // return redirect('https://a-trade.jp/redirect/berry?media=G16531'); //ベリー - Aトレード
