2eccc834d9fc86713c6e47e32752e16858e4e0b4
EC2 Default User <ec2-user@ip-172-31-3-105.us-east-2.compute.internal>
2021-02-11 23:25:34 +0000

    M

--- app/Http/Controllers/AdController.php
+++ app/Http/Controllers/AdController.php
@@ -10,10 +10,10 @@ class AdController extends Controller
     public function os(Request $request) {
         $user_agent =  $request->header('User-Agent');
         if ((strpos($user_agent, 'iPhone') !== false)) {
-            return redirect('https://a-trade.jp/redirect/mocom-lp30?media=G16531'); //モコム - Aトレード
+            return redirect('https://a-trade.jp/redirect/fatey-multi?media=G16531'); //FATEY - Aトレード
             // return redirect('https://a-trade.jp/redirect/popo?media=G16531'); //PoPo - Aトレード
         }
-        return redirect('https://a-trade.jp/redirect/mocom-lp30?media=G16531'); //モコム - Aトレード
+        return redirect('https://a-trade.jp/redirect/fatey-multi?media=G16531'); //FATEY - Aトレード
         // return redirect('https://a-trade.jp/redirect/popo?media=G16531'); //PoPo - Aトレード
     }
 }
