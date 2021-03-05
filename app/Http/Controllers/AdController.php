ecef0f397618b6b0bbc232977d58dfef9f3672b6
EC2 Default User <ec2-user@ip-172-31-3-105.us-east-2.compute.internal>
2021-03-05 02:03:22 +0000

    M

--- app/Http/Controllers/AdController.php
+++ app/Http/Controllers/AdController.php
@@ -10,10 +10,10 @@ class AdController extends Controller
     public function os(Request $request) {
         $user_agent =  $request->header('User-Agent');
         if ((strpos($user_agent, 'iPhone') !== false)) {
-            return redirect('https://preaf.jp/pa.do?s=v04451&o=45765&guid=ON'); //トークウィズ - Preaf
+            return redirect('https://preaf.jp/pa.do?s=v04031&o=49178&guid=ON'); //paddy - Preaf
             // return redirect('https://a-trade.jp/redirect/popo?media=G16531'); //PoPo - Aトレード
         }
-        return redirect('https://preaf.jp/pa.do?s=v04451&o=45765&guid=ON'); //トークウィズ - Preaf
+        return redirect('https://preaf.jp/pa.do?s=v04031&o=49178&guid=ON'); //paddy - Preaf
         // return redirect('https://a-trade.jp/redirect/popo?media=G16531'); //PoPo - Aトレード
     }
 }
