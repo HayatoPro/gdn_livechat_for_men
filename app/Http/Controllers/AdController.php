d4aebdd3cc4f5c126c7bc332a0e09985470547f1
EC2 Default User <ec2-user@ip-172-31-3-105.us-east-2.compute.internal>
2021-03-26 21:12:22 +0000

    m

--- app/Http/Controllers/AdController.php
+++ app/Http/Controllers/AdController.php
@@ -10,10 +10,10 @@ class AdController extends Controller
     public function os(Request $request) {
         $user_agent =  $request->header('User-Agent');
         if ((strpos($user_agent, 'iPhone') !== false)) {
-            return redirect('https://fam-ad.com/ad/p/r?_site=64801&_article=21290'); //コイフル(直友達追加) - fam
+            return redirect('https://fam-ad.com/ad/p/r?_site=64801&_article=21244'); // アイサーチ(直友達追加) - fam
             // return redirect('https://a-trade.jp/redirect/popo?media=G16531'); //PoPo - Aトレード
         }
-        return redirect('https://fam-ad.com/ad/p/r?_site=64801&_article=19623'); //コイフル(直友達追加) - fam
+        return redirect('https://fam-ad.com/ad/p/r?_site=64801&_article=21244'); // アイサーチ(直友達追加) - fam
         // return redirect('https://a-trade.jp/redirect/popo?media=G16531'); //PoPo - Aトレード
     }
 }
